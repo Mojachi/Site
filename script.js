d748f8aaa581e510477f2dca21849c7ac7998a70
Benjamin Pinter <ben.david.pinter@outlook.com>
2016-02-23 22:34:04 -0500

    Added new jquery to handle clicking header buttons. Now fades in new body text dependant on what the user clicked.

--- script.js
+++ script.js
@@ -1,10 +1,54 @@
 $(document).ready(function(){
+    
     $('.footerImage').hover(
         function(){
-            $(this).animate({'opacity':'.5'}, 100);        
+            $(this).animate({'opacity':'.5'}, 100);       
         },
         function() {
             $(this).animate({'opacity':'1'},100)
         }
     );
+    
+    $(".headerRowText").hover(
+        function() {
+            $(this).css("color", "White");        
+        },
+        function() {
+            $(this).css("color", "#333333");    
+        }
+    );
+    
+    $('.headerRowText').click(function() {
+            var currentInnerText = $(this).html();
+            $('#bottomBody').fadeOut('fast');
+            $('.footer').fadeOut('fast');
+            $('#topBody').fadeToggle('slow', function(){
+                if(currentInnerText === "About") {
+                    /*Turn this visibility calls into functions*/
+                    updateAndFade("BENJAMIN PINTER", "SOFTWARE DEVELOPER")
+                    $('.footer').fadeIn();                                    
+                } else if(currentInnerText === "Contact") {
+                    /*Turn this visibility calls into functions*/
+                    updateAndFade("Contact", "ben.david.pinter@gmail.com");
+                    $('.footer').fadeOut();
+                } else if(currentInnerText === "White Papers") {
+                    /*Turn this visibility calls into functions*/
+                    updateAndFade("White Papers", "Coming Soon!")
+                    $('.footer').fadeOut();
+                }
+                $('#topBody').fadeIn('slow');
+                $('#bottomBody').fadeIn('slow');
+            });    
+        }
+    );
+    
+    
 });
+
+function updateAndFade (newTopBody, newBottomBody) {
+    $('.footer').fadeOut();
+    $('#bottomBody').fadeOut('slow', function () {
+        $('#topBody').html(newTopBody);
+        $('#bottomBody').html(newBottomBody);    
+    });
+};
